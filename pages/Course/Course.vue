12af99ad1fdce73b5e5ef3973a8388591f8c2b6a
zyh223 <prassiacaesar@163.com>
2019-12-30 17:29:06 +0800

    Feat(course): add courseItem to course page
    #Close 11

--- pages/Course/Course.vue
+++ pages/Course/Course.vue
@@ -9,6 +9,14 @@
 					{{item}}
 				</view>
 			</view>
+			<view class="cu-list menu">
+				<view class="cu-item arrow" v-for="(item, index) in currentList" :key="index">
+					<view class="content">
+						<image :src="item.avatar" class="png" mode="aspectFit"></image>
+						<text class="text-grey" v-text="item.name"></text>
+					</view>
+				</view>
+			</view>
 		</scroll-view>
 	</view>
 </template>
@@ -19,15 +27,46 @@
 			return {
 				TabCur: 0,
 				scrollLeft: 0,
-				navigatorList: ["正在进行", "审核中", "往期课程"]
+				navigatorList: ["正在进行", "审核中", "往期课程"],
+				currentList: [{avatar:'/static/logo.png',name:'没刷新啊'}],
+					courseItemList: [
+						[{
+							avatar: '/static/logo.png',
+							name: '正在进行的课程一'
+						},{
+							avatar: '/static/logo.png',
+							name: '正在进行的课程二'
+						},{
+							avatar: '/static/logo.png',
+							name: '正在进行的课程三'
+						}],
+						[{
+							avatar: '/static/logo.png',
+							name: '正在审核的课程一'
+						},{
+							avatar: '/static/logo.png',
+							name: '正在审核的课程二'
+						}],
+						[{
+							avatar: '/static/logo.png',
+							name: '已经结束的课程一'
+						}]
+					]
 			};
 		},
 		methods: {
 			tabSelect(e) {
 				this.TabCur = e.currentTarget.dataset.id;
 				this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60
+				this.getCurrentList(e.currentTarget.dataset.id)
+			},
+			getCurrentList (i) {
+				this.currentList = this.courseItemList[i]
 			}
- 		}
+		},
+		mounted () {
+			this.getCurrentList(0)
+		}
 	}
 </script>
 
