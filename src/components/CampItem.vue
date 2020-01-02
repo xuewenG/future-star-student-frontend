842dbc3748ca214648243217619ddce7f5a6a263
nole <1361051747@qq.com>
2020-01-02 19:13:50 +0800

    Style(apply): change style code
    Ref #28

--- src/components/CampItem.vue
+++ src/components/CampItem.vue
@@ -1,68 +1,94 @@
 <template>
-    <view>
-      <swiper class="card-swiper square-dot" indicator-dots="true" circular="true" autoplay="true" interval="5000" duration="500" 
-      @change="cardSwiper" indicator-color="#8799a3" indicator-active-color="#0081ff">
-        <swiper-item v-for="(item, index) in campList" :key="index" :class="cardCur==index?'cur':''">
-          <view class="swiper-item">
-            <image :src="item.url" mode="aspectFill" v-if="item.type=='image'"></image>
-          </view>
-        </swiper-item>
-      </swiper>
-        
-      <view class="cu-list menu sm-border card-menu margin-top">
-        <view class="cu-item arrow" @tap="showCampInfo">
-          <view class="content">
-            <image src="../static/EdStarsLogo.png" class="png" mode="aspectFit"></image>
-            <text class="text-grey">创新院xx期描述</text>
-          </view>
+  <view>
+    <swiper
+      class="card-swiper square-dot"
+      indicator-dots="true"
+      circular="true"
+      autoplay="true"
+      interval="5000"
+      duration="500"
+      indicator-color="#8799a3"
+      indicator-active-color="#0081ff"
+      @change="cardSwiper"
+    >
+      <swiper-item
+        v-for="(item, index) in campList"
+        :key="index"
+        :class="cardCur==index?'cur':''"
+      >
+        <view class="swiper-item">
+          <image
+            v-if="item.type=='image'"
+            :src="item.url"
+            mode="aspectFill"
+          />
+        </view>
+      </swiper-item>
+    </swiper>
+
+    <view class="cu-list menu sm-border card-menu margin-top">
+      <view
+        class="cu-item arrow"
+        @tap="showCampInfo"
+      >
+        <view class="content">
+          <image
+            src="../static/EdStarsLogo.png"
+            class="png"
+            mode="aspectFit"
+          />
+          <text class="text-grey">
+            创新院xx期描述
+          </text>
         </view>
       </view>
     </view>
+  </view>
 </template>
 
 <script>
-    export default {
-      name: 'CampItem',
-      data () {
-        return {
-          campList: [{
-            campId: 0,
-            type: 'image',
-            url: '../static/EdStarsApply.png'
-          }, {
-            campId: 1,
-            type: 'image',
-            url: '../static/EdStarsApply.png',
-          }, {
-            campId: 2,
-            type: 'image',
-            url: '../static/EdStarsApply.png'
-          }, {
-            campId: 3,
-            type: 'image',
-            url: '../static/EdStarsApply.png'
-          }],
-          cardCur: 0
-        }
-      },
-      methods: {
-        cardSwiper(e) {
-        	this.cardCur = e.detail.current
-        },
-        showCampInfo(e) {
-          console.log(e)
-          uni.navigateTo({
-            url: '../pages/Apply/CampInfo?campId=' + this.cardCur,
-            fail: (res) => {
-              console.log(res)
-            },
-            success: (res) => {
-              console.log(res)
-            }
-          });
-        }
-      }
+export default {
+  name: 'CampItem',
+  data () {
+    return {
+      campList: [{
+        campId: 0,
+        type: 'image',
+        url: '../static/EdStarsApply.png'
+      }, {
+        campId: 1,
+        type: 'image',
+        url: '../static/EdStarsApply.png'
+      }, {
+        campId: 2,
+        type: 'image',
+        url: '../static/EdStarsApply.png'
+      }, {
+        campId: 3,
+        type: 'image',
+        url: '../static/EdStarsApply.png'
+      }],
+      cardCur: 0
     }
+  },
+  methods: {
+    cardSwiper (e) {
+      this.cardCur = e.detail.current
+    },
+    showCampInfo (e) {
+      console.log(e)
+      uni.navigateTo({
+        url: '../pages/Apply/CampInfo?campId=' + this.cardCur,
+        fail: (res) => {
+          console.log(res)
+        },
+        success: (res) => {
+          console.log(res)
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style>
--- src/pages/Apply/CampInfo.vue
+++ src/pages/Apply/CampInfo.vue
@@ -1,290 +1,303 @@
 <template>
   <view>
-    <cu-custom bgColor="bg-white" :isBack="true">
-      <block slot="backText">返回</block>
-      <block slot="content">未来之星</block>
+    <cu-custom
+      bg-color="bg-white"
+      :is-back="true"
+    >
+      <block slot="backText">
+        返回
+      </block>
+      <block slot="content">
+        未来之星
+      </block>
     </cu-custom>
-    
+
     <view class="cu-bar bg-white margin-top-sm">
       <view class="action">
-        <text class="cuIcon-titles text-orange"></text> 未来之星创新院xxx期招生中
+        <text class="cuIcon-titles text-orange" /> 未来之星创新院xxx期招生中
       </view>
     </view>
     <view class="cu-card case">
       <view class="cu-item shadow">
         <view class="image">
-          <image src="../../static/EdStarsApply.png" class="png" mode="widthFix"></image>
-          <view class="cu-tag bg-blue">招生</view>
+          <image
+            src="../../static/EdStarsApply.png"
+            class="png"
+            mode="widthFix"
+          />
+          <view class="cu-tag bg-blue">
+            招生
+          </view>
         </view>
         <view class="cu-item margin">
           <view class="text-content flex-sub">
-            <view v-html="camp.campInfo"></view>	
+            <!-- <view v-html="camp.campInfo" /> -->
           </view>
         </view>
       </view>
     </view>
-    
-    <view v-for="(clazz, index) in camp.classList" :key="index">
+
+    <view
+      v-for="(clazz, index) in camp.classList"
+      :key="index"
+    >
       <view class="cu-bar bg-white">
         <view class="action">
-          <text class="cuIcon-titles text-orange"></text> {{ clazz.className }}
+          <text class="cuIcon-titles text-orange" /> {{ clazz.className }}
         </view>
       </view>
       <view class="cu-card case">
         <view class="cu-item shadow">
           <view class="image">
-            <image :src="clazz.img" class="png" mode="widthFix"></image>
-            <view class="cu-tag bg-blue">{{ clazz.className }}</view>
+            <image
+              :src="clazz.img"
+              class="png"
+              mode="widthFix"
+            />
+            <view class="cu-tag bg-blue">
+              {{ clazz.className }}
+            </view>
           </view>
           <view class="cu-item margin">
             <view class="text-content flex-sub">
-              <view v-html="clazz.classInfo"></view>
+              <!-- <view v-html="clazz.classInfo" /> -->
             </view>
           </view>
-          
+
           <view class="cu-list menu sm-border card-menu margin-top">
-            <view v-for="(course, courseIndex) in clazz.courseList" :key="courseIndex" class="cu-item arrow" @tap="showClassInfo">
+            <view
+              v-for="(course, courseIndex) in clazz.courseList"
+              :key="courseIndex"
+              class="cu-item arrow"
+              @tap="showClassInfo"
+            >
               <view class="content">
-                <view class="cu-tag bg-cyan">{{ course.courseName + ' ' + course.courseType }}</view>
+                <view class="cu-tag bg-cyan">
+                  {{ course.courseName + ' ' + course.courseType }}
+                </view>
               </view>
             </view>
           </view>
         </view>
       </view>
     </view>
-    
+
     <view class="padding">
-      <button class="cu-btn bg-cyan shadow block" @tap="toApply">我要报名</button>  
+      <button
+        class="cu-btn bg-cyan shadow block"
+        @tap="toApply"
+      >
+        我要报名
+      </button>
     </view>
-    
   </view>
 </template>
 
 <script>
-  export default {
-    name: 'CampInfo',
-    data () {
-      return {
-        camp: {
-          campId: '',
-          campObject: '',
-          campInfo: '关于未来之星创新院<br>招生及报名<br>招生对象<br>申请介绍<br>',
-          campState: '',
-          classList: [
-            {
-              campId: '',
-              className: '素质教育专题班',
-              classInfo: '<h3>未来之星创新院——素质教育专题班”，为素质教育人量身打造的训练营，期待你的加入！</h3>' + 
+export default {
+  name: 'CampInfo',
+  data () {
+    return {
+      camp: {
+        campId: '',
+        campObject: '',
+        campInfo: '关于未来之星创新院<br>招生及报名<br>招生对象<br>申请介绍<br>',
+        campState: '',
+        classList: [
+          {
+            campId: '',
+            className: '素质教育专题班',
+            classInfo: '<h3>未来之星创新院——素质教育专题班”，为素质教育人量身打造的训练营，期待你的加入！</h3>' +
               '<p>素质教育专题班”有幸邀请到三位在素质教育领域有着丰富经验和深刻理解的专家顾问，他们将在课程内容设计上基于素质行业特点给予专业指导，为同学们带来专业、实用的专题课程</p>',
-              applyStart: '2019-09-01',
-              applyEnd: '2019-10-01',
-              enrollment: 50,
-              curEnroll: 20,
-              img: '../../static/EdStarsQualityClass.png',
-              courseList: [
-                {
-                  classId: '',
-                  courseName: '初心课',
-                  courseType: '大院通识课',
-                  courseInfo: '结合课堂讲授、游学参访、小组学习、沙盘、沙龙等多种学习方式，为素质教育创业者带来全方位的助力，全面提升创始人的认知',
-                  courseStart: '2019-07-01',
-                  courseEnd: '2019-07-03',
-                  courseSpot: '北京',
-                  modelList: [
-                    '了解前沿技术发展，探讨未来技术与教育的具体结合',
-                    '了解经济发展，消费升级给行业发展带来的机遇和挑战',
-                  ],
-                  lecturerList: [
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '李善友',
-                      title: '混沌大学创始人',
-                      briefInfo: '李善友是混沌大学创始人',
-                      tel: ''
-                    },
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '卫哲',
-                      title: '嘉御基金创始人',
-                      briefInfo: '卫哲是嘉御基金创始人',
-                      tel: ''
-                    },
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '李朝晖',
-                      title: '腾讯投资管理合伙人',
-                      briefInfo: '李朝晖是腾讯投资管理合伙人',
-                      tel: ''
-                    },
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '黄谭',
-                      title: '好未来首席技术官',
-                      briefInfo: '黄谭是好未来首席技术官',
-                      tel: ''
-                    }
-                  ]
-                },
-                
-                {
-                  classId: '',
-                  courseName: '创业课',
-                  courseType: '小班专业课',
-                  courseInfo: '创业课模块，紧贴细分赛道、解决实际业务问题',
-                  courseStart: '2019-09-01',
-                  courseEnd: '2019-09-03',
-                  courseSpot: '北京',
-                  modelList: [],
-                  lecturerList: []
-                },
-                {
-                  classId: '',
-                  courseName: '未来课',
-                  courseType: '大院通识课',
-                  courseInfo: '结合课堂讲授、游学参访、小组学习、沙盘、沙龙等多种学习方式，为素质教育创业者带来全方位的助力，全面提升创始人的认知',
-                  courseStart: '2019-11-01',
-                  courseEnd: '2019-11-03',
-                  courseSpot: '北京',
-                  modelList: [],
-                  lecturerList: []
-                }
-              ]
-            },
-            
-            {
-              campId: '',
-              className: '教育信息化专题班',
-              classInfo: '<h3>未来之星创新院——教育信息化专题班，为素质教育人量身打造的训练营，期待你的加入！</h3>',
-              applyStart: '2019-09-01',
-              applyEnd: '2019-10-01',
-              enrollment: 50,
-              curEnroll: 20,
-              img: '../../static/EdStarsEducationClass.png',
-              courseList: [
-                {
-                  classId: '',
-                  courseName: '初心课',
-                  courseType: '大院通识课',
-                  courseInfo: '结合课堂讲授、游学参访、小组学习、沙盘、沙龙等多种学习方式，为素质教育创业者带来全方位的助力，全面提升创始人的认知',
-                  courseStart: '2019-07-01',
-                  courseEnd: '2019-07-03',
-                  courseSpot: '北京',
-                  modelList: [
-                    '了解前沿技术发展，探讨未来技术与教育的具体结合',
-                    '了解经济发展，消费升级给行业发展带来的机遇和挑战',
-                  ],
-                  lecturerList: [
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '李善友',
-                      title: '混沌大学创始人',
-                      briefInfo: '李善友是混沌大学创始人',
-                      tel: ''
-                    },
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '卫哲',
-                      title: '嘉御基金创始人',
-                      briefInfo: '卫哲是嘉御基金创始人',
-                      tel: ''
-                    },
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '李朝晖',
-                      title: '腾讯投资管理合伙人',
-                      briefInfo: '李朝晖是腾讯投资管理合伙人',
-                      tel: ''
-                    },
-                    {
-                      lecturerId: '',
-                      avatar: '../../static/EdStarsLogo.png',
-                      lecturerName: '黄谭',
-                      title: '好未来首席技术官',
-                      briefInfo: '黄谭是好未来首席技术官',
-                      tel: ''
-                    }
-                  ]
-                },
-                {
-                  classId: '',
-                  courseName: '创业课',
-                  courseType: '小班专业课',
-                  courseStart: '',
-                  courseSpot: '',
-                  courseEnd: '',
-                  modelList: [],
-                  lecturerList: []
-                },
-                {
-                  classId: '',
-                  courseName: '未来课',
-                  courseType: '大院通识课',
-                  courseStart: '',
-                  courseSpot: '',
-                  courseEnd: '',
-                  modelList: [],
-                  lecturerList: []
-                }
-              ]
-            }
-          ]
-        },
-        clazz: {
-          campId: '',
-          className: '素质教育专题班',
-          classInfo: '<h3>未来之星创新院——素质教育专题班”，为素质教育人量身打造的训练营，期待你的加入！</h3>',
-          applyStart: '2019-09-01',
-          applyEnd: '2019-10-01',
-          enrollment: 50,
-          curEnroll: 20,
-          courseList: []
-        },
-        course: {
-          classId: '',
-          courseName: '',
-          courseInfo: '',
-          courseStart: '',
-          courseSpot: '',
-          courseEnd: '',
-          modelList: [],
-          lecturerList: []
-        },
-        lecturer: {
-          lecturerId: '',
-          avatar: '',
-          lecturerName: '',
-          title: '',
-          briefInfo: '',
-          tel: ''
-        }  
-      }
-    },
-    methods: {
-      toApply(e) {
-        console.log(e)
-        /* global uni:false */ 
-        uni.navigateTo({
-          url: '../Apply/ApplyForm',
-          fail: (res) => {
-            console.log(res)
+            applyStart: '2019-09-01',
+            applyEnd: '2019-10-01',
+            enrollment: 50,
+            curEnroll: 20,
+            img: '../../static/EdStarsQualityClass.png',
+            courseList: [
+              {
+                classId: '',
+                courseName: '初心课',
+                courseType: '大院通识课',
+                courseInfo: '结合课堂讲授、游学参访、小组学习、沙盘、沙龙等多种学习方式，为素质教育创业者带来全方位的助力，全面提升创始人的认知',
+                courseStart: '2019-07-01',
+                courseEnd: '2019-07-03',
+                courseSpot: '北京',
+                modelList: [
+                  '了解前沿技术发展，探讨未来技术与教育的具体结合',
+                  '了解经济发展，消费升级给行业发展带来的机遇和挑战'
+                ],
+                lecturerList: [
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '李善友',
+                    title: '混沌大学创始人',
+                    briefInfo: '李善友是混沌大学创始人',
+                    tel: ''
+                  },
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '卫哲',
+                    title: '嘉御基金创始人',
+                    briefInfo: '卫哲是嘉御基金创始人',
+                    tel: ''
+                  },
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '李朝晖',
+                    title: '腾讯投资管理合伙人',
+                    briefInfo: '李朝晖是腾讯投资管理合伙人',
+                    tel: ''
+                  },
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '黄谭',
+                    title: '好未来首席技术官',
+                    briefInfo: '黄谭是好未来首席技术官',
+                    tel: ''
+                  }
+                ]
+              },
+
+              {
+                classId: '',
+                courseName: '创业课',
+                courseType: '小班专业课',
+                courseInfo: '创业课模块，紧贴细分赛道、解决实际业务问题',
+                courseStart: '2019-09-01',
+                courseEnd: '2019-09-03',
+                courseSpot: '北京',
+                modelList: [],
+                lecturerList: []
+              },
+              {
+                classId: '',
+                courseName: '未来课',
+                courseType: '大院通识课',
+                courseInfo: '结合课堂讲授、游学参访、小组学习、沙盘、沙龙等多种学习方式，为素质教育创业者带来全方位的助力，全面提升创始人的认知',
+                courseStart: '2019-11-01',
+                courseEnd: '2019-11-03',
+                courseSpot: '北京',
+                modelList: [],
+                lecturerList: []
+              }
+            ]
           },
-          success: (res) => {
-            console.log(res)
-          },
-        });
+
+          {
+            campId: '',
+            className: '教育信息化专题班',
+            classInfo: '<h3>未来之星创新院——教育信息化专题班，为素质教育人量身打造的训练营，期待你的加入！</h3>',
+            applyStart: '2019-09-01',
+            applyEnd: '2019-10-01',
+            enrollment: 50,
+            curEnroll: 20,
+            img: '../../static/EdStarsEducationClass.png',
+            courseList: [
+              {
+                classId: '',
+                courseName: '初心课',
+                courseType: '大院通识课',
+                courseInfo: '结合课堂讲授、游学参访、小组学习、沙盘、沙龙等多种学习方式，为素质教育创业者带来全方位的助力，全面提升创始人的认知',
+                courseStart: '2019-07-01',
+                courseEnd: '2019-07-03',
+                courseSpot: '北京',
+                modelList: [
+                  '了解前沿技术发展，探讨未来技术与教育的具体结合',
+                  '了解经济发展，消费升级给行业发展带来的机遇和挑战'
+                ],
+                lecturerList: [
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '李善友',
+                    title: '混沌大学创始人',
+                    briefInfo: '李善友是混沌大学创始人',
+                    tel: ''
+                  },
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '卫哲',
+                    title: '嘉御基金创始人',
+                    briefInfo: '卫哲是嘉御基金创始人',
+                    tel: ''
+                  },
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '李朝晖',
+                    title: '腾讯投资管理合伙人',
+                    briefInfo: '李朝晖是腾讯投资管理合伙人',
+                    tel: ''
+                  },
+                  {
+                    lecturerId: '',
+                    avatar: '../../static/EdStarsLogo.png',
+                    lecturerName: '黄谭',
+                    title: '好未来首席技术官',
+                    briefInfo: '黄谭是好未来首席技术官',
+                    tel: ''
+                  }
+                ]
+              },
+              {
+                classId: '',
+                courseName: '创业课',
+                courseType: '小班专业课',
+                courseStart: '',
+                courseSpot: '',
+                courseEnd: '',
+                modelList: [],
+                lecturerList: []
+              },
+              {
+                classId: '',
+                courseName: '未来课',
+                courseType: '大院通识课',
+                courseStart: '',
+                courseSpot: '',
+                courseEnd: '',
+                modelList: [],
+                lecturerList: []
+              }
+            ]
+          }
+        ]
+      },
+      lecturer: {
+        lecturerId: '',
+        avatar: '',
+        lecturerName: '',
+        title: '',
+        briefInfo: '',
+        tel: ''
       }
-    },
-    onLoad: function (option) {
-      console.log(option.campId)
     }
+  },
+  methods: {
+    toApply (e) {
+      console.log(e)
+      /* global uni:false */
+      uni.navigateTo({
+        url: '../Apply/ApplyForm',
+        fail: (res) => {
+          console.log(res)
+        },
+        success: (res) => {
+          console.log(res)
+        }
+      })
+    }
+  },
+  onLoad: function (option) {
+    console.log(option.campId)
   }
+}
 </script>
 
 <style>
