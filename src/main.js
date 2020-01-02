9e08c31baecb13c7f4292d106ae9f0f0f905183f
xuewenG <gexuewen@outlook.com>
2020-01-02 18:13:32 +0800

    Chore(repo): add code style check CI
    Close #28

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App'
 
 import cuCustom from './colorui/components/cu-custom.vue'
-Vue.component('cu-custom',cuCustom)
+Vue.component('cu-custom', cuCustom)
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
