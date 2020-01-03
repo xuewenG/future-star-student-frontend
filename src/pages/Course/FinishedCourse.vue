d982f8dc9a201278a10bc5fb8bbf1ac6246c612e
zyh223 <prassiacaesar@163.com>
2020-01-03 16:22:56 +0800

    WIP: add FinishedCourse Page
    Empty content for now

--- src/pages/Course/FinishedCourse.vue
+++ src/pages/Course/FinishedCourse.vue
@@ -1,19 +1,31 @@
 <template>
   <view>
+    <TitleBar title="课程信息" />
+    <TopNav
+      :nav-list="navigatorList"
+      @cur-changed="(cur)=>{pageCur=cur}"
+    />
   </view>
 </template>
-
 <script>
+import TitleBar from '@/components/TitleBar.vue'
+import TopNav from '@/components/TopNav.vue'
 export default {
+  components: {
+    TopNav,
+    TitleBar
+  },
   data () {
     return {
+      pageCur: 0,
+      navigatorList: ['课程介绍', '课程资料', '同学列表']
     }
   },
-  methods: {
+  onLoad (option) {
+    console.log(option.id)
   }
 }
 </script>
 
 <style>
-
 </style>
