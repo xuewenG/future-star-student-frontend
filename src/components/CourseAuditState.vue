ff2177dfc84d3659ca2e9c06db28eb6799a6f00d
zyh223 <prassiacaesar@163.com>
2020-01-03 15:32:09 +0800

    WIP: add CourseAuditState Component(Used by AuditingCourse page)
    The current version is simple and the detailed one will be updated later

---
+++ src/components/CourseAuditState.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    <view
+      class="text-content padding bg-white"
+      v-text="auditState"
+    />
+  </view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      auditState: '您已通过初审，请等待电话面试通知'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/pages/Course/auditingCourse.vue
+++ src/pages/Course/auditingCourse.vue
@@ -1,18 +1,54 @@
 <template>
-  <view></view>
+  <view>
+    <TitleBar title="课程信息" />
+    <TopNav
+      :nav-list="navigatorList"
+      @cur-changed="(cur)=>{pageCur=cur}"
+    />
+    <course-info 
+      v-if="pageCur===0"
+      :course-data="courseData"
+    />
+    <course-audit-state
+      v-if="pageCur===1"
+      :course-resource-list="courseItemList"
+    />
+  </view>
 </template>
-
 <script>
+import TitleBar from '@/components/TitleBar.vue'
+import TopNav from '@/components/TopNav.vue'
+import CourseInfo from '@/components/CourseInfo.vue'
+import CourseAuditState from '@/components/CourseAuditState.vue'
 export default {
+  components: {
+    TopNav,
+    TitleBar,
+    CourseInfo,
+    CourseAuditState
+  },
   data () {
     return {
+      pageCur: 0,
+      navigatorList: ['课程介绍', '审核状态'],
+      courseData: {
+        time: '2020年1月1日——2020年2月15日',
+        place: '霸都飞禽繁育与生态研究中心',
+        intro: '这是一门课这是一门课这是一门课这是一门课这是一门课这是一门课这是一门课这是一门课这是一门课这是一门课这是一门课这是一门课',
+        teacher: {
+          name: '鸽老师',
+          avatar: '/static/tabbar/apply.png',
+          title: '2019年金鸽杯全国鸽手大赛“最佳鸽王”',
+          contact: 'Coo@666.com'
+        }
+      }
     }
   },
-  methods: {
+  onLoad (option) {
+    console.log(option.id)
   }
 }
 </script>
 
 <style>
-
 </style>
