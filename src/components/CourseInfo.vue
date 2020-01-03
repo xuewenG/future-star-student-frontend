832f8f3108999e1b95c52cdf4cb2f6209463abdf
zyh223 <prassiacaesar@163.com>
2020-01-03 14:50:34 +0800

    Refactor(course): extract CourseInfo components from OngoingCourse page
    #Ref 18

---
+++ src/components/CourseInfo.vue
@@ -0,0 +1,78 @@
+<template>
+  <view>
+    <cover-image src="/static/BasicsBg.png" />
+    <view class="cu-bar bg-white solid-bottom">
+      <view class="action">
+        <text class="cuIcon-title text-green" /> 课程介绍
+      </view>
+    </view>
+    <view class="text-content padding bg-white text-sm">
+      时间：{{ courseData.time }}<br>
+      地点：{{ courseData.place }}
+    </view>
+    <view
+      class="text-content padding bg-white"
+      v-text="courseData.intro"
+    >
+      暂无课程介绍
+    </view>
+    <view class="cu-bar bg-white solid-bottom margin-top">
+      <view class="action">
+        <text class="cuIcon-title text-green" /> 讲师
+      </view>
+    </view>
+    <view class="cu-list menu-avatar">
+      <view class="cu-item">
+        <view
+          class="cu-avatar round lg"
+          :style="'background-image:url('+courseData.teacher.avatar+');'"
+        />
+        <view class="content flex-sub">
+          <view v-text="courseData.teacher.name" />
+          <view
+            class="text-gray text-sm flex justify-between"
+            v-text="courseData.teacher.title"
+          >
+            暂无更多信息
+          </view>
+        </view>
+      </view>
+      <view class="cu-bar bg-white solid-bottom">
+        <view class="action">
+          <view class="text-sm">
+            联系方式：
+          </view>
+          <view
+            class="text-sm"
+            v-text="courseData.teacher.contact"
+          />
+        </view>
+      </view>
+      <view
+        class="text-content padding bg-white"
+        v-text="courseData.teacher.intro"
+      >
+        暂无讲师介绍
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    courseData: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Course/ongoingCourse.vue
+++ src/pages/Course/ongoingCourse.vue
@@ -5,63 +5,10 @@
       :nav-list="navigatorList"
       @cur-changed="(cur)=>{pageCur=cur}"
     />
-    <view v-if="pageCur===0">
-      <cover-image src="/static/BasicsBg.png" />
-      <view class="cu-bar bg-white solid-bottom">
-        <view class="action">
-          <text class="cuIcon-title text-green" /> 课程介绍
-        </view>
-      </view>
-      <view class="text-content padding bg-white text-sm">
-        时间：{{ courseData.time }}<br>
-        地点：{{ courseData.place }}
-      </view>
-      <view
-        class="text-content padding bg-white"
-        v-text="courseData.intro"
-      >
-        暂无课程介绍
-      </view>
-      <view class="cu-bar bg-white solid-bottom margin-top">
-        <view class="action">
-          <text class="cuIcon-title text-green" /> 讲师
-        </view>
-      </view>
-      <view class="cu-list menu-avatar">
-        <view class="cu-item">
-          <view
-            class="cu-avatar round lg"
-            :style="'background-image:url('+courseData.teacher.avatar+');'"
-          />
-          <view class="content flex-sub">
-            <view v-text="courseData.teacher.name" />
-            <view
-              class="text-gray text-sm flex justify-between"
-              v-text="courseData.teacher.title"
-            >
-              暂无更多信息
-            </view>
-          </view>
-        </view>
-        <view class="cu-bar bg-white solid-bottom">
-          <view class="action">
-            <view class="text-sm">
-              联系方式：
-            </view>
-            <view
-              class="text-sm"
-              v-text="courseData.teacher.contact"
-            />
-          </view>
-        </view>
-        <view
-          class="text-content padding bg-white"
-          v-text="courseData.teacher.intro"
-        >
-          暂无讲师介绍
-        </view>
-      </view>
-    </view>
+    <course-info 
+      v-if="pageCur===0"
+      :course-data="courseData"
+    />
     <view v-if="pageCur===1">
       <uni-collapse
         v-for="(item,index) in courseItemList"
@@ -107,12 +54,14 @@
 import TitleBar from '@/components/TitleBar.vue'
 import TopNav from '@/components/TopNav.vue'
 import { uniCollapse, uniCollapseItem } from '@dcloudio/uni-ui'
+import CourseInfo from '@/components/CourseInfo.vue'
 export default {
   components: {
     TopNav,
     TitleBar,
     uniCollapse,
-    uniCollapseItem
+    uniCollapseItem,
+    CourseInfo
   },
   data () {
     return {
