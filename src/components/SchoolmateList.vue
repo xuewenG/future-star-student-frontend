5abce09769a522ddf617a94a2b702ed0ee5b2862
nole <1361051747@qq.com>
2020-01-04 19:19:02 +0800

    Fix(schoolmate): adjust the position of the schoolmateList and area
    Close #20

--- src/components/SchoolmateList.vue
+++ src/components/SchoolmateList.vue
@@ -23,7 +23,7 @@
       scroll-y
       class="indexes"
       :scroll-into-view="'indexes-'+ groupCurId"
-      :style="[{height:'calc(100vh - '+ customBar + 'px - 50px)'}]"
+      :style="[{height:'calc(100vh - '+ customBar + 'px - ' + selectBar + 'px - 50px)'}, {top:selectBar + 'px'}]"
       :scroll-with-animation="true"
       :enable-back-to-top="true"
     >
@@ -100,12 +100,6 @@
 
 <script>
 export default {
-  // props: {
-  //   schoolmateList: {
-  //     type: Array,
-  //     required: true
-  //   }
-  // },
   name: 'SchoolmateList',
   data () {
     return {
@@ -162,10 +156,38 @@ export default {
               profession: '歌手'
             }
           ]
+        }, {
+          letter: 'C',
+          group: [
+            {
+              id: 7,
+              name: 'C阿黛尔',
+              avatar: '../../static/EdStarsLogo.png',
+              camp: '3',
+              area: '英国',
+              profession: '歌手'
+            }, {
+              id: 8,
+              name: 'C阿黛尔',
+              avatar: '../../static/EdStarsLogo.png',
+              camp: '3',
+              area: '英国',
+              profession: '歌手'
+            }, {
+              id: 9,
+              name: 'C阿黛尔',
+              avatar: '../../static/EdStarsLogo.png',
+              camp: '3',
+              area: '英国',
+              profession: '歌手'
+            }
+          ]
         }
       ],
       statusBar: this.StatusBar,
       customBar: this.CustomBar,
+      selectBar: '',
+      listBar: '',
       hidden: true,
       groupCurId: '',
       groupCur: ''
@@ -176,34 +198,37 @@ export default {
   },
   onReady () {
     const that = this
+    uni.createSelectorQuery().in(this).select('.cu-bar').boundingClientRect(function (res) {
+      that.selectBar = res.height
+    }).exec()
     uni.createSelectorQuery().in(this).select('.indexBar-box').boundingClientRect(function (res) {
-      console.log(res)
       that.boxTop = res.top
     }).exec()
     uni.createSelectorQuery().in(this).select('.indexes').boundingClientRect(function (res) {
-      console.log(res)
       that.barTop = res.top
     }).exec()
   },
   methods: {
     getSchoolmateInfo (id) {
-      // uni.navigateTo({
-      //   url: '../pages/Schoolmate/SchoolmateInfo?schoolmateId=' + id,
-      //   success: (res) => {
-      //     console.log(res)
-      //   },
-      //   fail: (res) => {
-      //     console.log(res)
-      //   }
-      // })
+      /* global uni:false */
+      uni.navigateTo({
+        url: '../pages/Schoolmate/SchoolmateInfo?schoolmateId=' + id,
+        success: (res) => {
+          console.log(res)
+        },
+        fail: (res) => {
+          console.log(res)
+        }
+      })
     },
     getCur (e) {
       this.hidden = false
       this.groupCur = this.schoolmateList[e.target.id].letter
     },
     setCur (e) {
+      const that = this
       this.hidden = true
-      this.groupCur = this.groupCurId
+      this.groupCur = that.groupCur
     },
     toStart () {
       this.hidden = false
@@ -220,6 +245,19 @@ export default {
         const num = parseInt((y - offsettop) / 20)
         this.groupCur = that.schoolmateList[num].letter
       }
+    },
+    indexSelect (e) {
+      const that = this
+      const barHeight = this.barHeight
+      const list = this.schoolmateList
+      const scrollY = Math.ceil(list.length * e.detail.y / barHeight)
+      for (let i = 0; i < list.length; i++) {
+        if (scrollY < i + 1) {
+          that.groupCur = list[i].letter
+          that.movableY = i * 20
+          return false
+        }
+      }
     }
   }
 }
@@ -232,65 +270,60 @@ export default {
 
 .indexBar {
   position: fixed;
-  right: 0px;
-  bottom: 0px;
-  padding: 20upx 20upx 20upx 60upx;
+  bottom: 0;
+  right: 0;
   display: flex;
   align-items: center;
+  padding: 20upx 20upx 20upx 60upx;
 }
 
 .indexBar .indexBar-box {
-  width: 40upx;
-  height: auto;
-  background: #fff;
   display: flex;
   flex-direction: column;
-  box-shadow: 0 0 20upx rgba(0, 0, 0, 0.1);
+  width: 40upx;
+  height: auto;
   border-radius: 10upx;
+  background: #fff;
+  box-shadow: 0 0 20upx rgba(0, 0, 0, .1);
 }
 
 .indexBar-item {
+  position: relative;
+  z-index: 9;
+  display: flex;
   flex: 1;
+  justify-content: center;
+  align-items: center;
   width: 40upx;
   height: 40upx;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   font-size: 24upx;
   color: #888;
 }
 
-movable-view.indexBar-item {
-  width: 40upx;
-  height: 40upx;
-  z-index: 9;
-  position: relative;
-}
-
-movable-view.indexBar-item::before {
-  content: "";
-  display: block;
+.indexBar-item::before {
   position: absolute;
-  left: 0;
   top: 10upx;
-  height: 20upx;
+  left: 0;
+  display: block;
   width: 4upx;
+  height: 20upx;
   background-color: #f37b1d;
+  content: "";
 }
 
 .indexToast {
   position: fixed;
   top: 0;
-  right: 80upx;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
+  right: 80upx;
   width: 100upx;
   height: 100upx;
-  border-radius: 10upx;
   margin: auto;
-  color: #fff;
+  border-radius: 10upx;
+  font-size: 48upx;
   line-height: 100upx;
   text-align: center;
-  font-size: 48upx;
+  color: #fff;
+  background: rgba(0, 0, 0, .5);
 }
 </style>
