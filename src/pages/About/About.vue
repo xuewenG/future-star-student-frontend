95bfea5a43f4c7646cde7a5e24b24c8aab99829e
zyh223 <prassiacaesar@163.com>
2020-01-05 15:53:57 +0800

    WIP: init the structure of about page
    ref #25

--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -1,10 +1,32 @@
 <template>
-  <view />
+  <view>
+    <view
+      class="margin-left content"
+    >
+      个人资料
+    </view>
+    <item-list
+      :list="aboutItem"
+    >
+    </item-list>
+  </view>
 </template>
 
 <script>
+import ItemList from '@/components/ItemList.vue'
 export default {
-
+  components: {
+    ItemList
+  },
+  data () {
+    return {
+      aboutItem: [{
+        id: 0,
+        name: '俞昊然',
+        intro: ''
+      }]
+    }
+  }
 }
 </script>
 
