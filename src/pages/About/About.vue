d9e2c98b1d34af48c6d315046f61920ff44b7f99
zyh223 <prassiacaesar@163.com>
2020-01-07 13:51:36 +0800

    WIP: complete the About page
    ref #25

--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -1,30 +1,61 @@
 <template>
   <view>
     <view
-      class="margin-left content"
+      class="cu-list menu sm-border card-menu"
     >
-      个人资料
+      <view
+        class="margin-sm text-sm text-gray content"
+      >
+        个人资料
+      </view>
+      <view
+        class="cu-item arrow margin-top"
+        @tap="navigateToAboutInfo(id)"
+      >
+        <view
+          class="cu-avatar round lg"
+          :style="'background-image:url('+aboutItem.img+');'"
+        />
+        <view class="margin-left content flex-sub">
+          <view v-text="aboutItem.name" />
+          <view
+            v-if="sub !== null"
+            class="text-gray text-sm flex justify-between"
+            v-text="aboutItem.intro"
+          >
+            暂无更多信息
+          </view>
+        </view>
+      </view>
     </view>
-    <item-list
-      :list="aboutItem"
-    >
-    </item-list>
   </view>
 </template>
 
 <script>
-import ItemList from '@/components/ItemList.vue'
 export default {
-  components: {
-    ItemList
-  },
   data () {
     return {
-      aboutItem: [{
+      aboutItem: {
         id: 0,
+        img: '/static/EdStarsLogo.png',
         name: '俞昊然',
         intro: ''
-      }]
+      }
+    }
+  },
+  methods: {
+    navigateToAboutInfo (id) {
+      const url = '/pages/About/PersonalInfo'
+      /* global uni:false */
+      uni.navigateTo({
+        url: url + '?id=' + id,
+        fail: function () {
+          console.log('fail to navigate to ' + url)
+        },
+        success: function () {
+          console.log('succeed to navigate to ' + url)
+        }
+      })
     }
   }
 }
