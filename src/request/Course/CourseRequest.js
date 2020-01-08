59cd81e76092921c9ef60115cc9139347b9b26cb
zyh223 <prassiacaesar@163.com>
2020-01-08 23:23:15 +0800

    WIP: init the auditingCourse request
    ref #47

--- src/request/Course/CourseRequest.js
+++ src/request/Course/CourseRequest.js
@@ -1,4 +1,5 @@
 import uniRequest from 'uni-request'
+import STATE from '@/request/constant'
 export default {
   async getOngoingCourseList (id) {
     console.log('getOngoingCourseList')
@@ -6,16 +7,46 @@ export default {
       const resp = await uniRequest.get('/clazz/clazz?page=1&page_size=999&student_id=' + id)
       if (resp.data.code === '2000') {
         let list = []
-        list = []
+        list = []// make a direct use of list in order to pass eslint
         for (let i = 0; i < parseInt(resp.data.data.count); i++) {
           const item = resp.data.data.results[i]
-          list.push({
-            id: item.id,
-            name: item.name,
-            img: '/static/EdStarsLogo.png',
-            intro: '',
-            url: '/pages/Course/OngoingCourse'
-          })
+          if (item.state === STATE.ACTIVITY.UNDERWAY) {
+            list.push({
+              id: item.id,
+              name: item.name,
+              img: '/static/EdStarsLogo.png',
+              intro: '',
+              url: '/pages/Course/OngoingCourse'
+            })
+          }
+        }
+        return list
+      } else {
+        console.error(resp.data.msg)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  async getAuditingCourseList (id) {
+    console.log('getAuditingCourseList')
+    try {
+      const resp = await uniRequest.get('/clazz/clazz?page=1&page_size=999&student_id=' + id)
+      if (resp.data.code === '2000') {
+        let list = []
+        list = []// make a direct use of list in order to pass eslint
+        for (let i = 0; i < parseInt(resp.data.data.count); i++) {
+          const item = resp.data.data.results[i]
+          if (item.state === 1) {
+            list.push({
+              id: item.id,
+              name: item.name,
+              img: '/static/EdStarsLogo.png',
+              intro: '',
+              url: '/pages/Course/OngoingCourse'
+            })
+          }
         }
         return list
       } else {
---
+++ src/request/constant.js
@@ -0,0 +1,10 @@
+export default {
+  ACTIVITY: {
+    UNOPENED: 0,
+    ENROLLING: 1,
+    BEFORE: 2,
+    UNDERWAY: 3,
+    CLOSED: 4,
+    CANCELLED: 5
+  }
+}
