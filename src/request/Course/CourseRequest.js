755ea4f7c7f9b95c4f3911926cccde79b09d01e2
zyh223 <prassiacaesar@163.com>
2020-01-08 22:15:33 +0800

    WIP: init the getOngoingCourseList request
    ref #46

--- src/request/Course/CourseRequest.js
+++ src/request/Course/CourseRequest.js
@@ -1,10 +1,26 @@
 import uniRequest from 'uni-request'
 export default {
-  async getUser () {
-    console.log('getUser')
+  async getOngoingCourseList (id) {
+    console.log('getOngoingCourseList')
     try {
-      const response = await uniRequest.get('/user?ID=12345')
-      console.log(response)
+      const resp = await uniRequest.get('/clazz/clazz?page=1&page_size=999&student_id=' + id)
+      if (resp.data.code === '2000') {
+        let list = []
+        list = []
+        for (let i = 0; i < parseInt(resp.data.data.count); i++) {
+          const item = resp.data.data.results[i]
+          list.push({
+            id: item.id,
+            name: item.name,
+            img: '/static/EdStarsLogo.png',
+            intro: '',
+            url: '/pages/Course/OngoingCourse'
+          })
+        }
+        return list
+      } else {
+        console.error(resp.data.msg)
+      }
     } catch (error) {
       console.error(error)
     }
