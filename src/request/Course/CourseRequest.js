8314245013631fb17447ff868d49b07b6a916dc0
zyh223 <prassiacaesar@163.com>
2020-01-08 23:30:25 +0800

    WIP: int the getFinishedCourseList request
    ref #49

--- src/request/Course/CourseRequest.js
+++ src/request/Course/CourseRequest.js
@@ -56,5 +56,34 @@ export default {
     } catch (error) {
       console.error(error)
     }
+  },
+  
+  async getAuditingCourseList (id) {
+    console.log('getAuditingCourseList')
+    try {
+      const resp = await uniRequest.get('/clazz/clazz?page=1&page_size=999&student_id=' + id)
+      if (resp.data.code === '2000') {
+        let list = []
+        list = []// make a direct use of list in order to pass eslint
+        for (let i = 0; i < parseInt(resp.data.data.count); i++) {
+          const item = resp.data.data.results[i]
+          console.log(item)
+          if (item.state === STATE.ACTIVITY.BEFORE) {
+            list.push({
+              id: item.id,
+              name: item.name,
+              img: '/static/EdStarsLogo.png',
+              intro: '',
+              url: '/pages/Course/AuditingCourse'
+            })
+          }
+        }
+        return list
+      } else {
+        console.error(resp.data.msg)
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
