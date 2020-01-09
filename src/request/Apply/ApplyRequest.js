642d25434c9216963b40b65b394be822b77f5ec0
nole <1361051747@qq.com>
2020-01-09 19:17:41 +0800

    Feat(apply): complete the api of getting classlist
    Ref #94

--- src/request/Apply/ApplyRequest.js
+++ src/request/Apply/ApplyRequest.js
@@ -32,5 +32,39 @@ export default {
     } catch (error) {
       console.error(error)
     }
+  },
+  async getCampInfo (campId) {
+    console.log('getCampInfo')
+    try {
+      const resp = await uniRequest.get('/clazz/clazz/page=1&page_size=999&semester_id=' + campId)
+      console.log(resp)
+      if (resp.data.code === '2000') {
+        console.log('获取学期的班级列表成功')
+        const results = resp.data.data.results
+        console.log(results)
+        let clazzList = []
+        clazzList = []
+        for (let i = 0; i < parseInt(resp.data.data.count); i++) {
+          const clazz = results[i]
+          clazzList.push({
+            classId: clazz.id,
+            className: clazz.name,
+            classInfo: clazz.introduction,
+            applyStart: clazz.start_time,
+            applyEnd: clazz.end_time,
+            enrollment: clazz.people_number_limit,
+            curEnroll: clazz.current_people_number,
+            state: parseInt(clazz.state),
+            img: '../../static/EdStarsQualityClass.png'
+          })
+        }
+        console.log(clazzList)
+        return clazzList
+      } else {
+        console.error(resp.data.msg)
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
