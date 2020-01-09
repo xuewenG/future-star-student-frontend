cd720ac467c223729904bd9d9c243538d04d9edf
zyh223 <prassiacaesar@163.com>
2020-01-09 17:53:05 +0800

    WIP: add clazz-student state constant
    ref #34

--- src/request/constant.js
+++ src/request/constant.js
@@ -2,6 +2,13 @@ export default {
   REQUEST: {
     SUCCESS: '2000'
   },
+  AUDIT: {
+    WAIT_FOR_AUDIT: 0,
+    REFUSED: 1,
+    PASSED: 2,
+    GRADUATED: 3,
+    UNABLE_FINISHED: 4
+  },
   ACTIVITY: {
     UNOPENED: 0,
     ENROLLING: 1,
