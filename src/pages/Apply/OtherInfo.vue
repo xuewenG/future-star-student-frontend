4da987b56ff35d263a30ed354bf54210a3bf9b32
nole <1361051747@qq.com>
2020-01-09 12:30:37 +0800

    Refactor(apply): pull the other information form away from the apply form
    Close #83

--- src/pages/Apply/OtherInfo.vue
+++ src/pages/Apply/OtherInfo.vue
@@ -1,8 +1,239 @@
 <template>
-  <view />
+  <view>
+    <cu-custom
+      bg-color="bg-white"
+      :is-back="true"
+    >
+      <block slot="backText">
+        返回
+      </block>
+      <block slot="content">
+        未来之星
+      </block>
+    </cu-custom>
+    <form>
+      <view class="cu-bar bg-white margin-top-sm solids-bottom">
+        <view class="action">
+          <text class="cuIcon-titles text-orange" /> 其他信息
+        </view>
+      </view>
+      <view class="cu-list menu">
+        <view class="cu-item">
+          <view class="content">
+            <image
+              src="../../static/icon/college.png"
+              class="png"
+              mode="aspectFit"
+            />
+            <text class="margin-left-sm text-center">
+              是否接受此考勤要求
+            </text>
+          </view>
+        </view>
+        <view class="cu-item text-gray">
+          <text>未来之星创新院名额宝贵，培训统一安排时间内，不接受因个人活动冲突的请假理由，如缺勤达到总课程的1/3将被劝退，您是否接受此考勤要求</text>
+        </view>
+        <radio-group
+          class="block"
+          @change="selectCheck"
+        >
+          <view class="cu-form-group">
+            <radio
+              class="blue radio margin-right-sm sm"
+              :class="otherForm.check==='accept'?'checked':''"
+              :checked="otherForm.check==='accept'?true:false"
+              value="接受"
+            />
+            <p class="margin-right">
+              接受
+            </p>
+            <radio
+              class="blue radio margin-right-sm sm"
+              :class="otherForm.check==='refuse'?'checked':''"
+              :checked="otherForm.check==='refuse'?true:false"
+              value="不接受"
+            />
+            <p>不接受</p>
+          </view>
+        </radio-group>
+        <view class="cu-item">
+          <view class="content">
+            <image
+              src="../../static/icon/applyReason.png"
+              class="png"
+              mode="aspectFit"
+            />
+            <text class="margin-left-sm">
+              请简述您的申请理由(100字以内)
+            </text>
+          </view>
+        </view>
+        <view class="cu-form-group cu-item align-start">
+          <textarea
+            v-model="otherForm.applyReason"
+            class="sm-border"
+            maxlength="100"
+            placeholder="请简述您的申请理由(100字以内)"
+            @blur="checkApplyReason"
+          />
+        </view>
+        <view class="cu-item">
+          <view class="content">
+            <image
+              src="../../static/icon/contribution.png"
+              class="png"
+              mode="aspectFit"
+            />
+            <text class="margin-left-sm">
+              您能为创新院的其他学员做出哪些贡献
+            </text>
+          </view>
+        </view>
+        <view class="cu-form-group cu-item align-start">
+          <textarea
+            v-model="otherForm.contributions"
+            class="sm-border"
+            maxlength="-1"
+            placeholder="请填写您能做出的贡献"
+          />
+        </view>
+        <view class="cu-item">
+          <view class="content">
+            <image
+              src="../../static/icon/canal.png"
+              class="png"
+              mode="aspectFit"
+            />
+            <text class="margin-left">
+              您通过何种渠道了解到“未来创新院”
+            </text>
+          </view>
+        </view>
+        <checkbox-group
+          class="block"
+          @change="selectCanals"
+        >
+          <view class="cu-form-group cu-list menu text-left">
+            <view
+              v-for="(canalItem, index) in canalList"
+              :key="index"
+              class="cu-item"
+            >
+              <label class="flex justify-between align-center flex-sub">
+                <checkbox
+                  :class="canalItem.checked?'checked':''"
+                  :checked="canalItem.checked?true:false"
+                  :value="canalItem.value"
+                />
+                <view class="flex-sub margin-left">
+                  {{ canalItem.value }}
+                </view>
+                <input
+                  v-if="canalItem.value==='其他'"
+                  v-model="otherCanal"
+                  placeholder="请输入您的了解渠道"
+                  name="input"
+                >
+              </label>
+            </view>
+          </view>
+        </checkbox-group>
+        <view class="cu-item flex">
+          <view @tap="checkBefore">
+            <text class="lg text-grey cuIcon-back" />
+            <text class="basis-xl margin-left content">
+              上一页
+            </text>
+          </view>
+          <view @tap="checkNext">
+            <text class="basis-xl margin-right content">
+              下一页
+            </text>
+            <text class="lg text-grey cuIcon-right" />
+          </view>
+        </view>
+      </view>
+      <view class="btn-area margin-top padding">
+        <button
+          formType="submit"
+          class="cu-btn bg-cyan shadow block"
+          @tap="submitApplyForm"
+        >
+          提交
+        </button>
+      </view>
+    </form>
+  </view>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      otherForm: {
+        check: 'accept',
+        applyReason: '',
+        contributions: '',
+        canals: []
+      },
+      canalList: [
+        {
+          value: '微信朋友圈',
+          checked: false
+        }, {
+          value: '朋友推荐',
+          checked: false
+        }, {
+          value: '未来之星公众号：EdStars未来同学会',
+          checked: false
+        }, {
+          value: '好未来官网',
+          checked: false
+        }, {
+          value: '媒体报道',
+          checked: false
+        }, {
+          value: '其他',
+          checked: false
+        }
+      ],
+      otherCanal: ''
+    }
+  },
+  methods: {
+    checkFormCheck (check) {
+    },
+    selectCheck (e) {
+      this.checkFormCheck(e.detail.value)
+    },
+    checkTextArea (text, info) {
+    },
+    checkApplyReason (e) {
+      this.checkTextArea(e.detail.value, '申请理由')
+    },
+    selectCanals (e) {
+      this.checkCanal(e.detail.value)
+    },
+    checkCanal (values) {
+    },
+    checkBefore () {
+      console.log('应返回推荐人页，同时保存当前页面数据')
+    },
+    checkNext () {
+      console.log(this.otherForm)
+      this.showToast('已经是最后一页，请提交')
+    },
+    showToast (info) {
+      uni.showToast({
+        title: info,
+        icon: 'none'
+      })
+    },
+    submitApplyForm () {
+      console.log(this.otherForm)
+    }
+  }
+}
 </script>
 
 <style>
