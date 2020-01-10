de2ad04d58f7a7c4985af388246ce690a01312f3
nole <1361051747@qq.com>
2020-01-10 13:12:21 +0800

    Feat(apply): complete the api of getting courselist through classId
    Ref #95

--- src/request/Apply/ApplyRequest.js
+++ src/request/Apply/ApplyRequest.js
@@ -1,14 +1,12 @@
 import uniRequest from 'uni-request'
+import { toChineseTimeString } from '@/request/util'
+
 export default {
   async getCampList () {
-    console.log('getCampList')
     try {
-      const resp = await uniRequest.get('/semester/semester')
-      console.log(resp)
+      const resp = await uniRequest.get('/semester/semester?page=1&page_size=999')
       if (resp.data.code === '2000') {
-        console.log('获取学期列表成功')
         const results = resp.data.data.results
-        console.log(results)
         let campList = []
         campList = []
         for (let i = 0; i < parseInt(resp.data.data.count); i++) {
@@ -19,12 +17,11 @@ export default {
             introduction: camp.introduction,
             state: camp.state,
             type: 'image',
-            swiperImg: '/static/EdStarsApply.png',
-            img: '/static/EdStarsLogo.png',
+            swiperImg: 'https://i.loli.net/2020/01/09/zkinxqPBwbtdvXQ.png',
+            img: 'https://i.loli.net/2020/01/09/iEZpONujHL4Sc13.png',
             url: '/pages/Apply/CampInfo'
           })
         }
-        console.log(campList)
         return campList
       } else {
         console.error(resp.data.msg)
@@ -34,32 +31,63 @@ export default {
     }
   },
   async getCampInfo (campId) {
-    console.log('getCampInfo')
     try {
-      const resp = await uniRequest.get('/clazz/clazz/page=1&page_size=999&semester_id=' + campId)
-      console.log(resp)
+      const resp = await uniRequest.get('/clazz/clazz?page=1&page_size=999&semester_id=' + campId)
       if (resp.data.code === '2000') {
-        console.log('获取学期的班级列表成功')
         const results = resp.data.data.results
-        console.log(results)
         let clazzList = []
         clazzList = []
         for (let i = 0; i < parseInt(resp.data.data.count); i++) {
           const clazz = results[i]
           clazzList.push({
-            classId: clazz.id,
-            className: clazz.name,
-            classInfo: clazz.introduction,
-            applyStart: clazz.start_time,
-            applyEnd: clazz.end_time,
+            id: clazz.id,
+            name: clazz.name,
+            info: clazz.introduction,
+            applyStart: toChineseTimeString(new Date(clazz.start_time)),
+            applyEnd: toChineseTimeString(new Date(clazz.end_time)),
             enrollment: clazz.people_number_limit,
             curEnroll: clazz.current_people_number,
+            admitted: clazz.people_number_limit !== clazz.current_people_number,
             state: parseInt(clazz.state),
-            img: '../../static/EdStarsQualityClass.png'
+            img: 'https://i.loli.net/2020/01/10/QD97zMkbIs1hw4d.png'
           })
         }
-        console.log(clazzList)
         return clazzList
+      } else {
+        console.error(resp.data.msg)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async getCourseList (classId) {
+    try {
+      const resp = await uniRequest.get('/course/course?page=1&page_size=999&clazz_id=' + classId)
+      if (resp.data.code === '2000') {
+        const results = resp.data.data.results
+        let courseList = []
+        courseList = []
+        for (let i = 0; i < parseInt(resp.data.data.count); i++) {
+          const course = results[i]
+          courseList.push({
+            id: course.id,
+            name: course.name,
+            type: course.type,
+            spot: course.location,
+            schedule: course.begin_time,
+            state: course.state,
+            moduleList: course.introduction.split(';'),
+            lecturer: {
+              id: course.teacher.id,
+              avatar: 'https://i.loli.net/2020/01/09/iEZpONujHL4Sc13.png',
+              name: course.teacher.name,
+              title: course.teacher.title,
+              introduction: course.teacher.introduction,
+              contact: course.teacher.contact_way
+            }
+          })
+        }
+        return courseList
       } else {
         console.error(resp.data.msg)
       }
