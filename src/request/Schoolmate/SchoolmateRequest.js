0c854905f8aad7ea3b3d2f67018d550532f5f142
nole <1361051747@qq.com>
2020-01-12 15:27:38 +0800

    Feat(schoolmate): complete the api of getting schoolmate information
    Close #98

--- src/request/Schoolmate/SchoolmateRequest.js
+++ src/request/Schoolmate/SchoolmateRequest.js
@@ -16,5 +16,27 @@ export default {
     } catch (error) {
       console.error(error)
     }
+  },
+  async getCampList () {
+    try {
+      const resp = await uniRequest.get('/semester/semester?page=1&page_size=999')
+      if (resp.data.code === STATE.REQUEST.SUCCESS) {
+        const results = resp.data.data.results
+        let campList = []
+        campList = []
+        for (let i = 0; i < parseInt(resp.data.data.count); i++) {
+          const camp = results[i]
+          campList.push({
+            title: camp.subject,
+            value: camp.id
+          })
+        }
+        return campList
+      } else {
+        console.error(resp.data.msg)
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
